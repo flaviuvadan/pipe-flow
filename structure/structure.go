1d99f4b4b61d4aecd2a064a91d48696d1d79bea0
FlaviuVadan <flaviuvadan@gmail.com>
2020-03-27 12:47:39 -0600

    Add description to structure

--- structure/structure.go
+++ structure/structure.go
@@ -3,26 +3,28 @@
 package structure
 
 import (
-	"github.com/flaviuvadan/pipe-flow/source"
-	"github.com/flaviuvadan/pipe-flow/sink"
-	"github.com/flaviuvadan/pipe-flow/pipe"
 	"github.com/flaviuvadan/pipe-flow/junction"
+	"github.com/flaviuvadan/pipe-flow/pipe"
+	"github.com/flaviuvadan/pipe-flow/sink"
+	"github.com/flaviuvadan/pipe-flow/source"
 )
 
 // Structure represents the state of the data processing system
 type Structure struct {
-	source    *source.Source
-	sink      *sink.Sink
-	pipes     []*pipe.Pipe
-	junctions []*junction.Junction
+	description string
+	source      *source.Source
+	sink        *sink.Sink
+	pipes       []*pipe.Pipe
+	junctions   []*junction.Junction
 }
 
 // New returns a new instance of a Structure
-func NewStructure(src *source.Source, snk *sink.Sink, pps []*pipe.Pipe, jnc []*junction.Junction) *Structure {
+func NewStructure(dsc string, src *source.Source, snk *sink.Sink, pps []*pipe.Pipe, jnc []*junction.Junction) *Structure {
 	return &Structure{
-		source:    src,
-		sink:      snk,
-		pipes:     pps,
-		junctions: jnc,
+		description: dsc,
+		source:      src,
+		sink:        snk,
+		pipes:       pps,
+		junctions:   jnc,
 	}
 }
