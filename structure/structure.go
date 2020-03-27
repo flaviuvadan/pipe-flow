0f5a828f0924562fbea3d02e3946169f528b3a61
FlaviuVadan <flaviuvadan@gmail.com>
2020-03-27 12:42:49 -0600

    Implement Structure New function

--- structure/structure.go
+++ structure/structure.go
@@ -2,11 +2,27 @@
 // starting and stopping
 package structure
 
+import (
+	"github.com/flaviuvadan/pipe-flow/source"
+	"github.com/flaviuvadan/pipe-flow/sink"
+	"github.com/flaviuvadan/pipe-flow/pipe"
+	"github.com/flaviuvadan/pipe-flow/junction"
+)
+
 // Structure represents the state of the data processing system
 type Structure struct {
+	source    *source.Source
+	sink      *sink.Sink
+	pipes     []*pipe.Pipe
+	junctions []*junction.Junction
 }
 
 // New returns a new instance of a Structure
-func NewStructure() *Structure {
-	return &Structure{}
+func NewStructure(src *source.Source, snk *sink.Sink, pps []*pipe.Pipe, jnc []*junction.Junction) *Structure {
+	return &Structure{
+		source:    src,
+		sink:      snk,
+		pipes:     pps,
+		junctions: jnc,
+	}
 }
