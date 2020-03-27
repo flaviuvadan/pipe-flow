5f7855c69edc4c87d0bbb42d376a1405cc4bcc75
FlaviuVadan <flaviuvadan@gmail.com>
2020-03-27 14:39:01 -0600

    Start building specs of the pipe

--- pipe/pipe.go
+++ pipe/pipe.go
@@ -3,9 +3,12 @@ package pipe
 
 // Pipe struct represents a pipeline through which data flows
 type Pipe struct {
+	input     map[string][]float64             // data that the pipe will apply the op to
+	operation func(map[string][]float64) error // the operation that will be applied to the input data
+	output    map[string][]float64             // the output after applying the operation to the input
 }
 
 // New returns a new instance of Pipe
-func NewPipe() *Pipe {
+func NewPipe(op func(map[string][]float64)) *Pipe {
 	return &Pipe{}
 }
