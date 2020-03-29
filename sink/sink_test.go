3b1e0704c78c5b81625e715347a664e036976bf4
FlaviuVadan <flaviuvadan@gmail.com>
2020-03-29 10:29:47 -0600

    Implement Dump tests

--- sink/sink_test.go
+++ sink/sink_test.go
@@ -2,6 +2,7 @@ package sink
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -98,6 +99,91 @@ func TestSink_Collect(t *testing.T) {
 			for _, m := range tt.pipesOut {
 				for k := range m {
 					assert.EqualValues(t, m[k], tt.expectedSinkData[k])
+				}
+			}
+		})
+	}
+}
+
+func TestSink_Dump(t *testing.T) {
+	tests := []struct {
+		name        string
+		filename    string
+		pipes       []*pipe.Pipe
+		pipesOut    []map[string][]float64
+		expectedErr error
+	}{
+		{
+			name:     "test_creates_empty_CSV_called_results",
+			filename: "",
+			pipes: []*pipe.Pipe{
+				pipe.NewPipe(nil),
+			},
+			pipesOut:    []map[string][]float64{{}},
+			expectedErr: nil,
+		},
+		{
+			name:     "test_creates_empty_CSV_called_filename",
+			filename: "test.csv",
+			pipes: []*pipe.Pipe{
+				pipe.NewPipe(nil),
+			},
+			pipesOut:    []map[string][]float64{{}},
+			expectedErr: nil,
+		},
+		{
+			name:     "test_creates_CSV_with_expected_content",
+			filename: "test_1.csv",
+			pipes: []*pipe.Pipe{
+				pipe.NewPipe(nil),
+			},
+			pipesOut: []map[string][]float64{
+				{
+					"a": {1, 2, 3},
+				},
+			},
+		},
+		{
+			name:     "test_creates_CSV_with_expected_extended_content",
+			filename: "test_2.csv",
+			pipes: []*pipe.Pipe{
+				pipe.NewPipe(nil),
+				pipe.NewPipe(nil),
+				pipe.NewPipe(nil),
+			},
+			pipesOut: []map[string][]float64{
+				{
+					"a": {1, 2, 3},
+				},
+				{
+					"b": {4, 5, 6},
+				},
+				{
+					"c": {7, 8, 9},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			for i, p := range tt.pipes {
+				p.SetOutput(tt.pipesOut[i])
+			}
+			s, _ := NewSink(tt.filename, tt.pipes)
+			err := s.Dump()
+			if err != nil {
+				assert.EqualError(t, err, tt.expectedErr.Error())
+			}
+			// TODO: this can benefit from making the source.read function a util one
+			// TODO: so other packages can read CSV files and check for equality between
+			// TODO: their maps and the maps built from reading the CSV
+			if tt.filename == "" {
+				if err := os.Remove("results.csv"); err != nil {
+					panic(fmt.Errorf("could not remove results.csv for tests teardown"))
+				}
+			} else {
+				if err := os.Remove(tt.filename); err != nil {
+					panic(fmt.Errorf("could not remove %v for tests teardown", tt.filename))
 				}
 			}
 		})
