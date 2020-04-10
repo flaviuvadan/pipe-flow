ae93dd4522b0964dfe623b616dd1a58cd705e43b
FlaviuVadan <flaviuvadan@gmail.com>
2020-04-10 13:37:43 -0600

    Remove GetState from junction.go
    Add SetNextInput to junction.go to allow setting of input to the next pipe

--- junction/junction.go
+++ junction/junction.go
@@ -3,32 +3,31 @@ package junction
 
 import (
 	"fmt"
-
 	"github.com/flaviuvadan/pipe-flow/pipe"
 )
 
 // Junction represents the structure of the junction
 type Junction struct {
-	in  *pipe.Pipe
-	out *pipe.Pipe
+	in  map[string][]float64 // results from the previous pipe
+	out *pipe.Pipe           // future pipe that runs on in
 }
 
 // New returns a new instance of a junction struct
-func NewJunction(in *pipe.Pipe, out *pipe.Pipe) *Junction {
+func NewJunction(out *pipe.Pipe) *Junction {
 	return &Junction{
-		in:  in,
 		out: out,
 	}
 }
 
-// GetState returns a formatted string that describes how long the in pipeline operated for
-func (j *Junction) GetState() string {
-	return fmt.Sprintf("IN pipeline description: %v | duration: %v\n"+
-		"OUT pipeline description: %v\n", j.in.Description, j.in.GetFlowDuration(), j.out.Description)
+// Continue makes the pipelines continue operating on the originally specified input
+func (j *Junction) Continue() (*Junction, error) {
+	if err := j.out.Flow(); err != nil {
+		return nil, fmt.Errorf("failed to continue flow, err: %v", err)
+	}
+	return j.out.GetJunction(), nil
 }
 
-// Continue makes the pipelines continue operating on the originally specified input
-func (j *Junction) Continue() error {
-	j.out.SetInput(j.in.GetOutput())
-	return j.out.Flow()
+// SetNextInput takes the given input and sets it as input to the future pipe
+func (j *Junction) SetNextInput(i map[string][]float64) {
+	j.out.SetInput(i)
 }
